309b6daf520d475a5d184dd734b91ba0675f5e6b
Ayush <aaaayush25@gmail.com>
2021-11-16 23:31:10 +0530

    upload app files

---
+++ app/marker_dictionary.js
@@ -0,0 +1,16 @@
+// Conatins map of marker ID to locations
+
+var taggedLoc = [];
+var num_loc = 1000;
+
+for(var i = 0; i < num_loc; i++) {
+    taggedLoc.push("Untagged");
+}
+
+taggedLoc[0] = "Test";
+taggedLoc[1] = "Maa chuda";
+taggedLoc[10] = "Drawing Room";
+taggedLoc[25] = "Canteen";
+taggedLoc[50] = "Central Hall";
+taggedLoc[60] = "Kitchen";
+taggedLoc[120] = "Men's Washroom";
