441b0deb8d8437e34ce0938243184dbcf6eb781c
Ayush <aaaayush25@gmail.com>
2021-12-02 20:40:09 +0530

    Add speech and object detection

---
+++ app/detector.js
@@ -0,0 +1,40 @@
+// let img;
+let video;
+let detector;
+let detections = [];
+
+function cocoPreload() {
+  return ml5.objectDetector('cocossd');
+}
+
+// function gotDetections(error, results) {
+//   if (error) {
+//     console.error(error);
+//   }
+//   detections = results;
+//   detector.detect(video, gotDetections);
+// }
+
+// function setup() {
+//   createCanvas(640, 480);
+//   video = createCapture(VIDEO);
+//   video.size(640, 480);
+//   video.hide();
+//   detector.detect(video, gotDetections);
+// }
+
+// function draw() {
+//   image(video, 0, 0);
+
+//   for (let i = 0; i < detections.length; i++) {
+//     let object = detections[i];
+//     stroke(0, 255, 0);
+//     strokeWeight(4);
+//     noFill();
+//     rect(object.x, object.y, object.width, object.height);
+//     noStroke();
+//     fill(255);
+//     textSize(24);
+//     text(object.label, object.x + 10, object.y + 24);
+//   }
+// }
