94b83b6230e551cf546dc667b550986cd54d30e8
Ayush <aaaayush25@gmail.com>
2021-12-03 11:57:06 +0530

    update dictionary

--- app/marker_dictionary.js
+++ app/marker_dictionary.js
@@ -11,14 +11,13 @@ taggedLoc[0] = "Test";
 taggedLoc[1] = "Bedroom 1";
 taggedLoc[2] = "Bedroom 2";
 taggedLoc[3] = "Kitchen";
-taggedLoc[4] = "Drawing Room";
+taggedLoc[4] = "Exit";
 taggedLoc[5] = "Guest Room";
 taggedLoc[6] = "Toilet";
 taggedLoc[7] = "Bathroom";
 taggedLoc[8] = "Staircase";
-
 taggedLoc[10] = "Drawing Room";
 taggedLoc[25] = "Canteen";
 taggedLoc[50] = "Central Hall";
-taggedLoc[60] = "Kitchen";
-taggedLoc[120] = "Men's Washroom";
+taggedLoc[60] = "Thank You!!";
+taggedLoc[100] = "This is Marker number 100";
